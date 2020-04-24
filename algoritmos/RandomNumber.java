e2ab37b8831c04cb87b4329f527117e9688d2043
Jose Hurtado <josehumberto17@gmail.com>
2020-04-23 21:11:05 -0500

    Create RandomNumber.java

---
+++ algoritmos/RandomNumber.java
@@ -0,0 +1,15 @@
+
+public class RandomNumber{
+
+
+public static void main(String[]args ){
+//4. Test [10...30]
+// min = 10 , max = 30, range = (max - min)
+//new Random().nextInt((max - min) + 1) + min
+new Random().nextInt((30 - 10) + 1) + 10
+//new Random().nextInt((20) + 1) + 10
+//new Random().nextInt(21) + 10    //[0...20] + 10 = [10...30]
+
+}
+
+}
